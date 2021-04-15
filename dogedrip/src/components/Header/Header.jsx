a023cfccd19d58c9bebeeac0747f6cf4620acfbf
Tiqur <trevorbrage02@gmail.com>
2021-04-15 00:04:38 -0700

    Header title

--- dogedrip/src/components/Header/Header.jsx
+++ dogedrip/src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import styles from './Header.module.scss'
 
 const Header = () => {
   return (
-    <Box boxShadow={4} className={styles.header} />
+    <Box boxShadow={4} className={styles.header}>
+      <p className={styles.title}>DogeDrip</p>
+    </Box>
   )
 }
 

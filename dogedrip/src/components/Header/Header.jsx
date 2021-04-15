873c4076a958123e6d9d11b57c8420e0664a5387
Tiqur <trevorbrage02@gmail.com>
2021-04-15 02:14:54 -0700

    Wallet input field

--- dogedrip/src/components/Header/Header.jsx
+++ dogedrip/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss'
 import DogePng from '../../assets/doge.png';
 const Header = () => {
   return (
-    <Box boxShadow={4} className={styles.header}>
+    <Box boxShadow={4} display='flex' className={styles.header}>
       <p className={styles.title}>DogeDrip</p>
       <img src={DogePng} className={styles.doge_png}/>
     </Box>
--- dogedrip/src/pages/Home/Home.jsx
+++ dogedrip/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import styles from './Home.module.scss';
 import Header from '../../components/Header/Header.jsx';
+import { TextField, Box, Container } from '@material-ui/core';
 
 
 const HomePage = (props) => {
   return (
-    <Header />
+    <Box display='flex' flexDirection='column'>
+      <Header />
+      <Container className={styles.address_container}>
+        <TextField className={styles.address_input} label='Enter your public wallet address here' variant='outlined' />
+      </Container>
+    </Box>
   )
 }
 

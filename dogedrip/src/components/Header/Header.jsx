f4e72bbe1fc40ad9f066d56280715566ca381a5b
Tiqur <trevorbrage02@gmail.com>
2021-04-15 00:10:29 -0700

    Doge Image on header

--- dogedrip/src/components/Header/Header.jsx
+++ dogedrip/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { Box } from '@material-ui/core';
 import styles from './Header.module.scss'
-
+import DogePng from '../../assets/doge.png';
 const Header = () => {
   return (
     <Box boxShadow={4} className={styles.header}>
       <p className={styles.title}>DogeDrip</p>
+      <img src={DogePng} className={styles.doge_png}/>
     </Box>
   )
 }

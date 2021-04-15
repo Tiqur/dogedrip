374b9596e342ddf9bb3c7a8bc51b61af36207a4e
Tiqur <trevorbrage02@gmail.com>
2021-04-15 03:26:45 -0700

    Basic buttons

--- dogedrip/src/pages/Home/Home.jsx
+++ dogedrip/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import styles from './Home.module.scss';
 import Header from '../../components/Header/Header.jsx';
-import { TextField, Box, Container } from '@material-ui/core';
+import { TextField, Box, Container, Button } from '@material-ui/core';
 
 
 const HomePage = (props) => {
@@ -9,6 +9,8 @@ const HomePage = (props) => {
       <Header />
       <Container className={styles.address_container}>
         <TextField className={styles.address_input} label='Enter your public wallet address here' variant='outlined' />
+        <Button varient='contained' color='primary'>Get free Doge!</Button>
+        <Button varient='outlined' color='secondary'>Donate to supply</Button>
       </Container>
     </Box>
   )

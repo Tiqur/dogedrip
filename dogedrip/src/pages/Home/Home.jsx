67d20b24d528bc5ada585db398c7aef28fe82002
Tiqur <trevorbrage02@gmail.com>
2021-04-15 03:32:26 -0700

    Updated index.html

--- dogedrip/src/pages/Home/Home.jsx
+++ dogedrip/src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { TextField, Box, Container, Button } from '@material-ui/core';
 
 const HomePage = (props) => {
   return (
-    <Box display='flex' flexDirection='column'>
+    <Box flex flexDirection='column'>
       <Header />
       <Container className={styles.address_container}>
         <TextField className={styles.address_input} label='Enter your public wallet address here' variant='outlined' />

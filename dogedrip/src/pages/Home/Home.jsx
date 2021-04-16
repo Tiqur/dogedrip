3d1103cc42121afa7591bfb850fbbe6bdcc5aae1
Tiqur <trevorbrage02@gmail.com>
2021-04-15 21:35:09 -0700

    Aligned buttons

--- dogedrip/src/pages/Home/Home.jsx
+++ dogedrip/src/pages/Home/Home.jsx
@@ -9,8 +9,12 @@ const HomePage = (props) => {
       <Header />
       <Container className={styles.address_container}>
         <TextField className={styles.address_input} label='Enter your public wallet address here' variant='outlined' />
-        <Button variant='contained' color='primary'>Get free Doge!</Button>
-        <Button variant='outlined' color='secondary'>Donate to supply</Button>
+        <Box display='flex' justifyContent='center' marginTop='3em'>
+          <Box display='flex' justifyContent='space-between' width='25em'>
+            <Button variant='contained' color='primary'>Get free Doge!</Button>
+            <Button variant='outlined' color='secondary'>Donate to supply</Button>
+          </Box>
+        </Box>
       </Container>
     </Box>
   )

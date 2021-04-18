6ba3a5db3bd4b3c71d60516d3ba5157665c6108e
Tiqur <trevorbrage02@gmail.com>
2021-04-18 01:19:23 -0700

    Added backdrop and fixed z-index

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,22 @@
 import styles from './Home.module.scss';
 import Header from '../../components/Header/Header.jsx';
-import { Snackbar, TextField, Box, Typography, Button } from '@material-ui/core';
+import { Backdrop, Snackbar, TextField, Box, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import MuiAlert from '@material-ui/lab/Alert';
 import { useRef, useState } from 'react';
 import axios from 'axios';
 
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1
+  }
+}));
 
 const HomePage = (props) => {
   const walletAddressRef = useRef(null);
   const [snackBarState, setSnackBarState] = useState(false);
   const [addressIsValid, setAddressIsValid] = useState(false);
+  const classes = useStyles();
   const supply = 430.23;
 
 
@@ -22,6 +29,12 @@ const HomePage = (props) => {
 
   return (
     <Box display='flex' alignItems='center' flexDirection='column'>
+
+      { /* Donation popup */ }
+      <Backdrop className={classes.backdrop} open={true}>
+
+      </Backdrop>
+
       <Header />
       <Box display='flex' flexDirection='column' alignItems='center' className={styles.container}>
         
